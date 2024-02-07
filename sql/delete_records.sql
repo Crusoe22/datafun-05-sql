fd88d77fc1c83e6ae21d906178f6904b66d201b1
Nolan Moss <s573653@nwmissouri.edu>
2024-02-06 20:51:05 -0500

    update sql queries

--- sql/delete_records.sql
+++ sql/delete_records.sql
@@ -1,5 +1,4 @@
 -- SQL file to delete 1 record from a table
 
 -- Delete a record from 'books'
-DELETE FROM books
-WHERE title = 'To Kill a Mockingbird';
+DELETE FROM books WHERE title = 'The Art of War';
--- sql/update_records.sql
+++ sql/update_records.sql
@@ -1,6 +1,6 @@
 -- SQL file to update 1 record in a table
 
--- Update a record in 'vcrs'
-UPDATE vcrs
-SET availability = 1
-WHERE model = 'VCR-1002';
+-- Update a record in 'books'
+UPDATE books 
+SET year_published = '1719'
+WHERE title = 'Robinson Crusoe';
